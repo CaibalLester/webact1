0ad61fbfefe5c6530708135f4e9db85d3eb64410
CaibalLester <caiballester4@gmail.com>
2023-10-06 01:12:37 +0800

    update

--- app/controllers/User.php
+++ app/controllers/User.php
@@ -6,39 +6,98 @@ class User extends Controller {
     public function __construct()
     {
         parent:: __construct();
-        $this->call->model('user_model');
+        $this->call->model('User_model');
     }
 	
-
-    public function aboutUS()
+    public function ShowData() 
     {
-        echo "Testing";
-    }
-
-    public function index()
-    {
-        $this->call->model('user_model');
-        $data['users'] = $this->user_model->getUsers();
+        $data = [
+            'users' => $this->User_model->Getuser(),
+        ];
         $this->call->view('about_us', $data);
     }
 
-    public function add_account()
+    public function Save() 
     {
-        $data =  $this->user_model->insert_data($this->io->post('username'), $this->io->post('password'), $this->io->post('email'), $this->io->post('usertype'));
-        $this->call->view('about_us', $data);
-    }
-
-    public function delete_data()
-    {
-        if($this->user_model->delete_data())
-            redirect('/about_us');
-            exit;
-    }
-
+        $id = $_POST['id'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $email = $_POST['email'];
+        $usertype = $_POST['usertype'];
         
+        if($id != null) 
+        {
+            $result = $this->User_model->Updateuser($id, $username, $password, $email, $usertype);
+            if($result) 
+            {
+                $data = [
+                    'users' => $this->User_model->Getuser(),
+                    'msg' => 'Updated!',
+                ];
+                $this->call->view('about_us', $data);
+            }
+            else
+            {
+                $data = [
+                    'users' => $this->User_model->Getuser(),
+                    'msg' => 'Error!',
+                ];
+                $this->call->view('about_us', $data);
+            }
+        }
+        else
+        {
+            $result = $this->User_model->Saveuser($username, $password, $email, $usertype);
+            if($result) 
+            {
+                $data = [
+                    'users' => $this->User_model->Getuser(),
+                    'msg' => 'Saved!',
+                ];
+                $this->call->view('about_us', $data);
+            }
+            else
+            {
+                $data = [
+                    'users' => $this->User_model->Getuser(),
+                    'msg' => 'Error!',
+                ];
+                $this->call->view('about_us', $data);
+            }
+        };
     }
 
-   
+    public function Delete($id) 
+    {
+        $ID = $id;
+        $result = $this->User_model->Deleteuser($ID);
+        if($result) 
+        {
+            $data = [
+                'users' => $this->User_model->Getuser(),
+                'msg' => 'Deleted!',
+            ];
+            $this->call->view('about_us', $data);
+        }
+        else
+        {
+            $data = [
+                'users' => $this->User_model->Getuser(),
+                'msg' => 'Error!',
+            ];
+            $this->call->view('about_us', $data);
+        }
+    }
 
+    public function Edit($id) 
+    {
+        $ID = $id;
+        $data = [
+            'select_us' => $this->User_model->Selectuser($ID), 
+            'users' => $this->User_model->Getuser(),
+        ];
 
+        $this->call->view('about_us', $data);
+    }
+    }
 ?>

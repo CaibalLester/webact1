0b88a024e12c7b10d301fbb5866c1cb62c24315f
CaibalLester <caiballester4@gmail.com>
2023-10-06 01:02:00 +0800

    update

--- app/models/User_model.php
+++ app/models/User_model.php
@@ -3,13 +3,27 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 class User_model extends Model {
 	
-    public function getUser()
+    public function Getuser()
     {
         $data = $this->db->table('users')->get_all();
 		return $data;
     }
 
-    public function save($username, $password, $email, $usertype)
+    public function Selectuser($id)
+    {
+        $ID = $id;
+        $data = $this->db->table('users')->where('id',$ID)->get();
+		return $data;
+    }
+
+    public function Deleteuser($id)
+    {
+        $ID = $id;
+        $data = $this->db->table('users')->where('id',$ID)->delete();
+		return $data;
+    }
+
+    public function Saveuser($username, $password, $email, $usertype)
     {
         $data = [
             'username' => $username,
@@ -20,31 +34,16 @@ class User_model extends Model {
         return $this->db->table('users')->insert($data);
     }
 
-    public function select($id)
+    public function Updateuser($Id, $username, $password, $email, $usertype)
     {
-        $userId = $id;
-        $data = $this->db->table('users')->where('id',$userId)->get();
-		return $data;
-    }
-
-    public function update($id, $username, $password, $email, $usertype)
-    {
-        $userId = $id;
+        $ID = $Id;
         $data = [
             'username' => $username,
             'password' => $password,
             'email' => $email,
             'usertype' => $usertype,
         ];
-        return $this->db->table('users')->where('id',$userId)->update($data);
+        return $this->db->table('users')->where('id',$ID)->update($data);
     }
-
-    public function delete($id)
-    {
-        $userId = $id;
-        $data = $this->db->table('users')->where('id',$userId)->delete();
-		return $data;
-    }
-
 }
 ?>
